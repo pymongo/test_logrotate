947b5c7affbf950b801856fa16fb6cf1403745c4
w <os.popen@gmail.com>
2021-04-02 17:20:33 +0800

    fix logrotate config wrong comment syntax

--- install_test_logrotate_user_service.sh
+++ install_test_logrotate_user_service.sh
@@ -14,14 +14,19 @@ EOF
 systemctl --user daemon-reload
 systemctl --user status test_logrotate
 
-
 # add logrotate config
 sudo tee /etc/logrotate.d/test_logrotate <<EOF >/dev/null
 /home/w/workspace/temp/test_logrotate/log/*.log {
     daily
-    rotate 30 // 只保留最近30天的日志
-    dateext // 日志分割分卷时按日期命名(默认只会按xxx.1和xxx.2这样命名)
+    # 只保留最近30天的日志(注意注释一定只能单独一行，不能在配置的行末加注释)
+    rotate 30
+    # 将日志拷贝一份进行分卷压缩，再清空原日志(类似ruby的log4r)
+    copytruncate
+    # 日志分割分卷时按日期命名(默认只会按xxx.1和xxx.2这样命名)
+    dateext
     dateformat -%Y%m%d
     compress
 }
 EOF
+# test log_rotate config file
+sudo logrotate /etc/logrotate.d/test_logrotate
